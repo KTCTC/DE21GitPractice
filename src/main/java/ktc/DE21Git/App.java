e24132f935ec486280972e66bb3e372251f01316
KTCTC <ktcorporatetrainingcentre@gmail.com>
2022-05-11 21:54:21 +0530

    first commit

---
+++ src/main/java/ktc/DE21Git/App.java
@@ -0,0 +1,13 @@
+package ktc.DE21Git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
